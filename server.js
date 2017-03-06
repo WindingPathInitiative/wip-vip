f6f812e5320787d97ef90d671c8eba5821fa87b1
Ephraim Gregor <egregor@usatoday.com>
2017-03-06 16:05:11 -0500

    PRES-24: Installs partial response middleware.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: false }) );
+app.use( require( 'express-partial-response' )() );
 app.use( require( 'morgan' )( 'dev' ) );
 
 app.use( '/v1/awards/', require( './endpoints/awards' ).route() );
