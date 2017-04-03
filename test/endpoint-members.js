b02328c3fe2cf6b18b44d439e13aef66acb1a2da
Ephraim Gregor <egregor@usatoday.com>
2017-04-03 12:58:35 -0400

    Adds tests for all filter types.

--- test/endpoint-members.js
+++ test/endpoint-members.js
@@ -137,100 +137,108 @@ module.exports = function() {
 		});
 	});
 
-	describe( 'GET /v1/members/{user}/prestige', function() {
+	[ null, 'prestige', 'vip' ].forEach( type => {
+		let name = type;
+		if ( ! type ) {
+			name = 'awards'
+		}
+		describe( `GET /v1/members/{user}/${name}`, function() {
 
-		it( 'returns all awards with user me', function( done ) {
-			new MembersEndpoint( null, 1 )
-			.get( 'me', { status: 'all' }, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'returns all awards with user me', function( done ) {
+				new MembersEndpoint( null, 1 )
+				.get( 'me', { status: 'all' }, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					awards.results.length.should.be.greaterThan( 0 );
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
-		});
 
-		it( 'returns all awards with user set to self', function( done ) {
-			new MembersEndpoint( null, 1 )
-			.get( 1, { status: 'all' }, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'returns all awards with user set to self', function( done ) {
+				new MembersEndpoint( null, 1 )
+				.get( 1, { status: 'all' }, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					awards.results.length.should.be.greaterThan( 0 );
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
-		});
 
-		it( 'returns awards for default status', function( done ) {
-			new MembersEndpoint( null, 2 )
-			.get( 1, {}, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'returns awards for default status', function( done ) {
+				new MembersEndpoint( null, 2 )
+				.get( 1, {}, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					awards.results.length.should.be.greaterThan( 0 );
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
-		});
 
-		it( 'fails if checking all awards without permission', function( done ) {
-			new MembersEndpoint( hub( 403 ), 2 )
-			.get( 1, { status: 'all' }, 'prestige' )
-			.catch( err => {
-				err.should.be.an.Error().and.an.instanceOf( errors.AuthError );
-				done();
+			it( 'fails if checking all awards without permission', function( done ) {
+				new MembersEndpoint( hub( 403 ), 2 )
+				.get( 1, { status: 'all' }, type )
+				.catch( err => {
+					err.should.be.an.Error().and.an.instanceOf( errors.AuthError );
+					done();
+				});
 			});
-		});
 
-		it( 'fails if checking pending awards without permission', function( done ) {
-			new MembersEndpoint( hub( 403 ), 2 )
-			.get( 1, { status: 'Requested' }, 'prestige' )
-			.catch( err => {
-				err.should.be.an.Error().and.an.instanceOf( errors.AuthError );
-				done();
+			it( 'fails if checking pending awards without permission', function( done ) {
+				new MembersEndpoint( hub( 403 ), 2 )
+				.get( 1, { status: 'Requested' }, type )
+				.catch( err => {
+					err.should.be.an.Error().and.an.instanceOf( errors.AuthError );
+					done();
+				});
 			});
-		});
 
-		it( 'works if checking awards with permission', function( done ) {
-			new MembersEndpoint( hub(), 2 )
-			.get( 1, { status: 'all' }, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'works if checking awards with permission', function( done ) {
+				new MembersEndpoint( hub(), 2 )
+				.get( 1, { status: 'all' }, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					awards.results.length.should.be.greaterThan( 0 );
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
-		});
 
-		it( 'can filter by before date', function( done ) {
-			new MembersEndpoint( null, 2 )
-			.get( 1, { dateBefore: '2017-02-21' }, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'can filter by before date', function( done ) {
+				new MembersEndpoint( null, 2 )
+				.get( 1, { dateBefore: '2017-02-21' }, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					awards.results.length.should.be.greaterThan( 0 );
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
-		});
 
-		it( 'can filter by after date', function( done ) {
-			new MembersEndpoint( null, 2 )
-			.get( 1, { dateAfter: '2017-02-21' }, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'can filter by after date', function( done ) {
+				new MembersEndpoint( null, 2 )
+				.get( 1, { dateAfter: '2017-02-21' }, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					if ( 'prestige' === type ) {
+						awards.results.length.should.be.greaterThan( 0 );
+					}
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
-		});
 
-		it( 'can limit result size', function( done ) {
-			new MembersEndpoint( null, 2 )
-			.get( 1, { limit: 1 }, 'prestige' )
-			.then( awards => {
-				awards.results.should.be.an.Array();
-				awards.results.length.should.be.greaterThan( 0 );
-				awards.results.forEach( validateAward );
-				done();
+			it( 'can limit result size', function( done ) {
+				new MembersEndpoint( null, 2 )
+				.get( 1, { limit: 1 }, type )
+				.then( awards => {
+					awards.results.should.be.an.Array();
+					awards.results.length.should.be.greaterThan( 0 );
+					awards.results.forEach( validateAward );
+					done();
+				});
 			});
 		});
 	});
